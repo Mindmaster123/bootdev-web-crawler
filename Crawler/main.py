732542495ae8dff04383b0d997d7f06c054ab962
mindmaster <apaninja666@gmail.com>
2026-04-05 22:58:21 +0200

    added some comments

--- Crawler/main.py
+++ Crawler/main.py
@@ -14,11 +14,11 @@ async def main_async():
     max_concurrency = int(sys.argv[2])
     max_pages = int(sys.argv[3])
     craw = await crawl_site_async(sys.argv[1], max_concurrency, max_pages)
-    for page in craw.values(): #this shit is broken cause page is none well its not broken if u don't limit pages so something fucky example if site has 32 links and u limit it to 32 it works fine but limit it to 31 and u get 2 of the 31 links in this test case
-        if page is None: continue
-        print(page["url"])
+    #for page in craw.values(): #this shit is broken cause page is none well its not broken if u don't limit pages so something fucky example if site has 32 links and u limit it to 32 it works fine but limit it to 31 and u get 2 of the 31 links in this test case
+     #   if page is None: continue #this needs to not be here for pending to work tho since it skips none wish is what becomes pending
+      #  print(page["url"] if page else "pending") # this makes it say pending instead of nothing wish is bad? good?
     print(len(craw))
-
+    #could not all this be done inside crawl.py ?_?
 
 
     #for page in craw.values():

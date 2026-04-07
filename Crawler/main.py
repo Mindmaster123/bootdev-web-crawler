c2f28ee1ba592c84a22730d133b2887990ecef06
mindmaster <apaninja666@gmail.com>
2026-04-07 20:52:07 +0200

    cleaner?

--- Crawler/main.py
+++ Crawler/main.py
@@ -9,9 +9,6 @@ async def main_async():
     if len(sys.argv) > 4:
         print("too many arguments provided")
         sys.exit(1)
-    if len(sys.argv) == 2:
-        print(f"starting crawl of: {sys.argv[1]}")
-    #craw = crawl_page(sys.argv[1])
     max_concurrency = int(sys.argv[2])
     max_pages = int(sys.argv[3])
     craw = await crawl_site_async(sys.argv[1], max_concurrency, max_pages)
